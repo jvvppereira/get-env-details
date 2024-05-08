bfcb50240965122bb7f79f2a855d73a470f843dc
Pereira <joao.paes@philips.com>
2024-05-08 08:32:26 -0300

    created first MVP

---
+++ server.js
@@ -0,0 +1,40 @@
+import  fetch  from "node-fetch";
+import  express  from "express";
+import  cors  from "cors";
+import  bodyParser  from "body-parser";
+
+const app = express();
+const port = 4000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/:env/:start/:end', async (req, res) => {
+    const { env, start, end } = req.params;
+
+    const allEnvs = [];
+
+    for (let i = start; i <= end; i++) {
+        const environmentURL = `https://${env.replace("00", i)}`;
+        const versionJSON = environmentURL + '/version.json';
+
+        const envInfo = await(await fetch(versionJSON)).json();    
+        
+        allEnvs.push({
+            url: environmentURL,
+            version: envInfo.tasy,
+        });
+    }
+
+    res.send(allEnvs);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});

8f047a068ae26618ee716bfe650329981f34a80c
Pereira <joao.paes@philips.com>
2024-06-19 11:10:59 -0300

    added the unavailable envs on response

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import  fetch  from "node-fetch";
-import  express  from "express";
-import  cors  from "cors";
-import  bodyParser  from "body-parser";
+import fetch from "node-fetch";
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 4000;
@@ -14,25 +14,29 @@ app.get('/:app/:env/:start/:end', async (req, res) => {
     const { app, env, start, end } = req.params;
 
     const allEnvs = [];
+    const unavailableEnvs = [];
 
     for (let i = start; i <= end; i++) {
         const environmentURL = `https://${env.replace("00", i)}`;
         const versionJSON = environmentURL + '/version.json';
 
         try {
-            const envInfo = await(await fetch(versionJSON)).json();    
-            
+            const envInfo = await (await fetch(versionJSON)).json();
+
             allEnvs.push({
                 url: environmentURL,
                 version: envInfo[app],
             });
         } catch (error) {
+            unavailableEnvs.push({
+                url: environmentURL
+            });
             console.log('error to load: ', versionJSON);
             console.log(error);
         }
     }
 
-    res.send(allEnvs);
+    res.send({ available: allEnvs, unavailable: unavailableEnvs });
 });
 
 app.listen(port, () => {
